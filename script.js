c95f4e797a045f767cd86ef8a9698c7468e514df
27038122 <27038122@sun.ac.za>
2025-02-15 17:39:38 +0200

    nicer UI

---
+++ script.js
@@ -0,0 +1,11 @@
+document.getElementById("confirmButton").addEventListener("click", function () {
+    emailjs.send("service_b54w34f", "template_qup48ia", {
+      message: "Plants have been watered",
+      to_email: "janesmostert@gmail.com" // Replace with your email
+    })
+      .then(function (response) {
+        alert("Notification sent!");
+      }, function (error) {
+        alert("Error sending notification");
+      });
+  });
